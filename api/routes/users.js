ac9d204b3d3a754abe2522cef10b2941c23a40fc
lowlyHero <e.nunn1009@gmail.com>
2019-07-29 16:54:42 -0400

    renamed auth file and wrote JWT token generaterwith auth function

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
+const db = require('../../data/dbConfig');
+const bcrypt = require('bcryptjs');
 
-const Users = require('../../data/helpers/userDb');
-
-router.get('/', (req, res) => {
-
+router.get('/', async (req, res) => {
+    try {
+        res.status(200).json({ message: 'sanity == good' });
+    } catch(e) {
+        res.status(500).json({ message: 'Internal Error. Please try again.' })
+    }
 });
 
 module.exports = router;
