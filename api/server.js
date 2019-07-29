d233c66f4790cc38bad8ce579f1fcbd54e2ad624
lowlyHero <e.nunn1009@gmail.com>
2019-07-29 15:24:59 -0400

    updated package.json for jest testing. added config folder with secrets file

--- api/server.js
+++ api/server.js
@@ -1,17 +1,18 @@
 const express = require('express'); 
+const helmet = require('helmet');
 const cors = require('cors');
 
 // Import Routers Here
 
 const server = express();
 
+server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
 // Make endpoints here
-// server.use('/login', authRouter); 
-// server.use('/signup', router);
-// server.use('/users', router)
+// server.use('/api/auth', authRouter); 
+// server.use('/api/users', router)
 
 server.get('/', (req, res) => {
     res.send('<h1>Sanity == Good</h1>');
